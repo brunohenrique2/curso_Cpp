d9a0b926532a6a143785bd6258870f86b6077001
Bruno Soares <72941170+brunohenrique2@users.noreply.github.com>
2025-01-20 23:05:04 -0300

    aula 05

---
+++ aula_05/codigo03.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+#define pi 3.1415
+#define balas_max cout << "100\n"
+
+int main() {
+
+    int vida, balas_current, balas_damage, balas_speed;
+    cout << pi << "\n\n";
+    balas_max;
+    system("pause");
+    return 0;
+}
